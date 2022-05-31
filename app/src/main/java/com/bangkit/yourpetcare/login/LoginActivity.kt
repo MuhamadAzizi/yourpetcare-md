e8f75662e4a4df95502e83ad8e82f774d3b34f2b
vandarma27 <firman.27@students.amikom.ac.id>
2022-06-01 00:10:54 +0700

    fitur login done

--- app/src/main/java/com/bangkit/yourpetcare/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/yourpetcare/login/LoginActivity.kt
@@ -1,10 +1,18 @@
 package com.bangkit.yourpetcare.login
 
+import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bangkit.yourpetcare.R
+import com.bangkit.yourpetcare.beranda.MainActivity
 import com.bangkit.yourpetcare.databinding.ActivityLoginBinding
 import com.bangkit.yourpetcare.register.RegisterActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
     private var binding : ActivityLoginBinding? = null
@@ -14,11 +22,97 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        autoLogin()
 
         binding?.tvBlmpunyaakun?.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
+        binding?.login?.setOnClickListener {
+            formValidation()
+        }
+    }
 
+    private fun autoLogin() {
+        if(FirebaseAuth.getInstance().currentUser!=null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun formValidation() {
+        var email = binding?.emailLogin?.text.toString().trim()
+        var password = binding?.passwordLogin?.text.toString().trim()
+
+        if(email.isEmpty()){
+            Toast.makeText(this, "Email jangan dikosongkan", Toast.LENGTH_SHORT).show()
+            return
+        }else if(password.isEmpty()){
+            Toast.makeText(this, "Password jangan dikosongkan", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Mohon ditunggu terlebih dahulu")
+            progressDialog.setCanceledOnTouchOutside(false)
+            progressDialog.show()
+
+        FirebaseFirestore
+            .getInstance()
+            .collection("users")
+            .get()
+            .addOnCompleteListener(OnCompleteListener{task ->
+                if(task.result.size() == 0){
+                    progressDialog.dismiss()
+                    failureDialog()
+                    return@OnCompleteListener
+                    }
+
+                for(snapshot in task.result){
+                    val email = "" + snapshot ["email"]
+                    FirebaseAuth
+                        .getInstance()
+                        .signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if(task.isSuccessful){
+                                progressDialog.dismiss()
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            }else {
+                                progressDialog.dismiss()
+                                failureDialog()
+                            }                        }
+                    }
+                }
+            )
+    }
+
+    private fun failureDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Berhasil Login")
+            .setMessage("Selamat anda berhasil login")
+            .setIcon(R.drawable.ic_baseline_check_24)
+            .setPositiveButton("oke"){
+                dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun successDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Gagal Login")
+            .setMessage("Mohon maaf, anda gagal login, cek koneksi internet anda")
+            .setIcon(R.drawable.ic_baseline_clear_24)
+            .setPositiveButton("oke"){
+                dialog, _ ->
+                dialog.dismiss()
+                onBackPressed()
+            }.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding
     }
 }
--- app/src/main/java/com/bangkit/yourpetcare/register/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/yourpetcare/register/RegisterActivity.kt
@@ -1,11 +1,16 @@
 package com.bangkit.yourpetcare.register
 
+import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bangkit.yourpetcare.R
 import com.bangkit.yourpetcare.databinding.ActivityRegisterBinding
 import com.bangkit.yourpetcare.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,6 +25,97 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(Intent(this,LoginActivity::class.java))
         }
 
-
+        binding?.register?.setOnClickListener {
+            formValidation()
+        }
     }
+
+    private fun formValidation(){
+        val username = binding?.usernameRegist?.text.toString().trim()
+        val email = binding?.emailRegist?.text.toString().trim()
+        val password = binding?.passwordRegist?.text.toString().trim()
+
+        if(username.isEmpty()){
+            Toast.makeText(this, "Username jangan kosong", Toast.LENGTH_SHORT).show()
+            return
+        }else if(email.isEmpty()){
+            Toast.makeText(this, "Email jangan kosong", Toast.LENGTH_SHORT).show()
+            return
+        }else if(password.isEmpty()){
+            Toast.makeText(this, "Password jangan kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Mohon ditunggu terlebih dahulu")
+        progressDialog.setCanceledOnTouchOutside(false)
+        progressDialog.show()
+
+
+        FirebaseAuth
+            .getInstance()
+            .createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                    val data = mapOf(
+                        "uid" to uid,
+                        "username" to username,
+                        "email" to email,
+                        "password" to password
+                    )
+
+                    uid.let {
+                        data2-> FirebaseFirestore
+                        .getInstance()
+                        .collection("users")
+                        .document(data2)
+                        .set(data)
+                        .addOnCompleteListener { task ->
+                            if(task.isSuccessful){
+                                progressDialog.dismiss()
+                                successDialog()
+                            }else{
+                                progressDialog.dismiss()
+                                failureDialog()
+                            }
+                        }
+                    }
+
+                }else{
+                    progressDialog.dismiss()
+                    failureDialog()
+
+                }
+            }
+    }
+
+    private fun failureDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Gagal Daftar")
+            .setMessage("Terdapat kesalahan saat melakukan pendaftaran, silahkan cek koneksi internet anda")
+            .setIcon(R.drawable.ic_baseline_clear_24)
+            .setPositiveButton("Oke"){
+                dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun successDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Berhasil Daftar")
+            .setMessage("Selamat anda berhasil mendaftar, silahkan login")
+            .setIcon(R.drawable.ic_baseline_check_24)
+            .setPositiveButton("Oke"){
+                dialog, _ ->
+                dialog.dismiss()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding=null
+    }
+
 }
